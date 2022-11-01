0a78779c801976a6f9227bf4668902d8ce6317e7
izuutech <izutechsofficial@gmail.com>
2022-11-01 22:17:46 +0100

    add create email waitlist

--- components/EmailInput.js
+++ components/EmailInput.js
@@ -1,16 +1,40 @@
+import { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { addToWaitlist } from "../services/endpoints";
+
 export default function EmailInput() {
+  const [email, setEmail] = useState("");
+
+  const submit = async (e) => {
+    e.preventDefault();
+    if (email) {
+      const data = await addToWaitlist({ email });
+      if (data?.data?.message) {
+        toast.success(data?.data?.message, { autoClose: 3000 });
+      } else {
+        toast.error(data?.data?.error, { autoClose: 3000 });
+      }
+    } else {
+      toast.warning("Please input your email address", { autoClose: 3000 });
+    }
+  };
   return (
-    <div className="w-full flex justify-center my-4">
+    <form onSubmit={submit} className="w-full flex justify-center my-4">
       <div className="rounded-full w-full md:w-[60%] flex bg-slate-100 overflow-hidden">
         <input
           type="email"
           className="w-full py-2 px-4 bg-transparent outline-none"
           placeholder="Email address"
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
         />
         <button className="bg-[#00983D] text-white py-2 px-4 w-2/4 md:w-1/4 rounded-full">
           Subscribe
         </button>
       </div>
-    </div>
+      <ToastContainer />
+    </form>
   );
 }
---
+++ services/endpoints.js
@@ -0,0 +1,29 @@
+import axios from "axios";
+
+const BASE_URL = "http://localhost:5000";
+
+export const addToWaitlist = async (values) => {
+  try {
+    const res = await axios.post(
+      `${BASE_URL}/emails`,
+      { email: values.email },
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    return res;
+  } catch (e) {
+    return e.response;
+  }
+};
+
+export const fetchList = async () => {
+  try {
+    const res = await axios.get(`${BASE_URL}/emails`);
+    return res;
+  } catch (error) {
+    console.log(error);
+  }
+};
