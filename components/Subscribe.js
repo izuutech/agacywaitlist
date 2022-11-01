c1758c1ad8180fe01fbc51c7441f1376a769e0d4
izuutech <izutechsofficial@gmail.com>
2022-11-01 21:12:59 +0100

    finish ui for landing1

--- components/Subscribe.js
+++ components/Subscribe.js
@@ -3,12 +3,10 @@ import EmailInput from "./EmailInput";
 export default function Subscribe() {
   return (
     <div className="w-full my-12">
-      <h1 className="font-bold text-center text-2xl">
-        Sign Up for our Newsletter
-      </h1>
+      <h1 className="font-bold text-center text-2xl">Join our waitlist</h1>
       <div className="w-[90%] md:w-[60%] ml-[5%] md:ml-[20%] text-center">
-        Join our newsletter to get exclusive contents, offers, services and
-        first access to all our products. You also get to be notified when we
+        Join our waitlist to get exclusive contents, offers, services and first
+        access to all our products. You also get to be notified first, when we
         launch!
       </div>
       <EmailInput />
